cb8c16fe8d0c148597f903ff362912ae592959f3
Its-Isaku <Isai.almeraz@uabc.edu.mx>
2026-01-07 19:55:33 -0800

    105-Assignment#1

---
+++ BookManager/Models/Book.swift
@@ -0,0 +1,15 @@
+//
+//  Book.swift
+//  BookManager
+//
+//  Created by Isai Magdaleno Almeraz Landeros on 07/01/26.
+//
+
+import Foundation
+
+struct Book: Hashable {
+	var tittle: String
+	var author: String
+	var details: String
+	var cover: String
+}
